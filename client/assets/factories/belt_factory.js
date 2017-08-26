e639c249e8d3e2c0cd03212a271d803bfd7cb940
hkhattar <himanshikhatter@gmail.com>
2017-08-25 19:51:25 -0700

    initial master

--- client/assets/factories/belt_factory.js
+++ client/assets/factories/belt_factory.js
@@ -9,6 +9,7 @@ app.factory('belt_factory',function($http){
             // console.log(user.name + " in factory")
             console.log('user',user)
             var data = {name:user}
+            console.log('data',data)
             $http.post('/createuser', data).success(function(output){
               console.log('output',output)
               callback(output)
